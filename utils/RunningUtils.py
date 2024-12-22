c77fe34ce244cc952f63ce398671ffe2883d90a4
Cees Melis <cees3331x00@gmail.com>
2024-12-22 11:27:43 +0100

    Refactor running utils and add blob test with pingas

---
+++ utils/RunningUtils.py
@@ -0,0 +1,11 @@
+from utils.GraphVisualizer import GraphVisualizer
+from algorithms.BoruvkasAlgorithm import BoruvkasAlgorithm
+
+def do_boruvka_on_graph(graph, graph_name, iterations = 10):
+    gv = GraphVisualizer(graph)
+    gv.plot_graph(graph_name)
+
+    max_iterations = iterations
+    plot_graph = lambda i: gv.plot_graph(f'{graph_name} boruvkas round {i}')
+    alg = BoruvkasAlgorithm(graph, max_iterations, False, plot_graph)
+    alg.run();
