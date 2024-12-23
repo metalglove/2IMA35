bd567e1d63060ef7ae281c4703af5fd24500cb0a
Mario Lucassen <Mario.lucassen@hotmail.com>
2024-12-23 19:15:26 +0100

    added cluster parameter

--- subplot_runner.py
+++ subplot_runner.py
@@ -12,7 +12,7 @@ from utils.GraphGenerator import GraphGenerator
 from utils.GraphVisualizer import GraphVisualizer
 from algorithms.BoruvkasAlgorithm import BoruvkasAlgorithm
 
-def boruvkas(clean, name, points, ax):
+def boruvkas(clean, name, points, n_clusters, ax):
     if clean:
         # clean
         print(f'\n==== BORUVKAS ALGORITHM (CLEAN DATA) ====\n')
@@ -21,7 +21,7 @@ def boruvkas(clean, name, points, ax):
         gv = GraphVisualizer(G, True, dir = get_runner_string(name, clean, 'runner'))
 
         def plot(i):
-            if len(gv.G.V) >= 3:
+            if len(gv.G.V) >= n_clusters:
                 gv.plot_neighborhoods(f'{name} boruvkas round {i}', ax=ax)
 
         max_iterations = 30
@@ -34,7 +34,7 @@ def boruvkas(clean, name, points, ax):
         gv = GraphVisualizer(G, True, dir = get_runner_string(name, clean, 'runner'))
 
         def plot(i):
-            if len(gv.G.V) >= 3:
+            if len(gv.G.V) >= n_clusters:
                 gv.plot_neighborhoods(f'{name} boruvkas round {i} (noise)', ax=ax)
 
         max_iterations = 30
@@ -43,6 +43,7 @@ def boruvkas(clean, name, points, ax):
 
 
 def kmeans(clean, name, points, n_clusters, ax):
+    voronoi = n_clusters >= 3
     if clean:
         # clean
         print(f'\n==== KMEANS ALGORITHM (CLEAN DATA) ====\n')
@@ -52,7 +53,7 @@ def kmeans(clean, name, points, n_clusters, ax):
         gv = GraphVisualizer(G, True, dir = get_runner_string(name, clean, 'runner'))
 
         def plot(i, centroids):
-            gv.plot_kmeans(f'{name} kmeans round {i}', centroids=centroids, voronoi=True, ax=ax)
+            gv.plot_kmeans(f'{name} kmeans round {i}', centroids=centroids, voronoi=voronoi, ax=ax)
 
         max_iterations = 300
         alg = KMeansAlgorithm(G, max_iterations, n_clusters, False, lambda i, centroids: plot(i, centroids))
@@ -65,13 +66,13 @@ def kmeans(clean, name, points, n_clusters, ax):
         gv = GraphVisualizer(G, True, dir = get_runner_string(name, clean, 'runner'))
         
         def plot(i, centroids):
-            gv.plot_kmeans(f'{name} kmeans round {i} (noise)', centroids=centroids, voronoi=True, ax=ax)
+            gv.plot_kmeans(f'{name} kmeans round {i} (noise)', centroids=centroids, voronoi = voronoi, ax=ax)
 
         max_iterations = 300
         alg = KMeansAlgorithm(G, max_iterations, n_clusters, False, lambda i, centroids: plot(i, centroids))
         alg.run()
 
-def singlelink(clean, name, points, ax):
+def singlelink(clean, name, points, n_clusters, ax):
     if clean:
         # clean
         print(f'\n==== SLAC ALGORITHM (CLEAN DATA) ====\n')
@@ -83,7 +84,7 @@ def singlelink(clean, name, points, ax):
         max_iterations = len(G.V)
 
         def plot(i):
-            if len([component for component in gv.G.components if type(component) is Component]) >= 3:
+            if len([component for component in gv.G.components if type(component) is Component]) >= n_clusters:
                 gv.plot_component_graph(f'{name} SLAC round {i}', ax=ax)
 
         alg = SingleLinkAgglomerativeClusteringAlgorithm(G, max_iterations, False, plot)
@@ -99,7 +100,7 @@ def singlelink(clean, name, points, ax):
         max_iterations = len(G.V)
 
         def plot(i):
-            if len([component for component in gv.G.components if type(component) is Component]) >= 3:
+            if len([component for component in gv.G.components if type(component) is Component]) >= n_clusters:
                 gv.plot_component_graph(f'{name} SLAC round {i} (with noise)', ax=ax)
 
         alg = SingleLinkAgglomerativeClusteringAlgorithm(G, max_iterations, False, plot)
@@ -107,30 +108,30 @@ def singlelink(clean, name, points, ax):
         alg.run(start_plotting_at_n_components)
 
 def get_noise(points):
-        # .add_gaussian_noise(0.25) \
-        # .draw_line((-1, 5), (0, -6.), 1.5) \
-        # .add_clustered_noise('circle', 0.1) \
         # .add_point((8, 7)) \
+        # .add_point((7, 7)) \
+        # .add_clustered_noise('circle', 0.1) \
+        # .draw_line((-1, 5), (0, -6.), 1.5) \
     G = GraphGenerator(points.copy()) \
-        .add_point((7, 7)) \
+        .add_gaussian_noise(0.1) \
         .to_graph(gen_pair_wise=True)
     return G
 
 def run():
-    dataset_names = ['blobs'] 
+    dataset_names = ['circles'] 
     # dataset_names = ['ans1c'] 
     # dataset_names = ['ans1a', 'ans1b', 'ans1c',] 
     # dataset_names = ['ans1a', 'ans1b', 'ans1c', 'blobs', 'moons', 'circles', 'maccie']
     n_samples = 500
-    n_clusters = 3
-    for clean in [True]: # True
+    n_clusters = 2
+    for clean in [True, False]: # True
         for dataset in dataset_names:
             fig = plt.figure(figsize=(16, 4))
             axs = fig.subplots(1, 3, sharex=True, sharey=True)
             points = generate_points(dataset, n_samples, n_clusters)
             kmeans(clean, dataset, points, n_clusters, axs[1])
-            boruvkas(clean, dataset, points, axs[0])
-            singlelink(clean, dataset, points, axs[2])
+            boruvkas(clean, dataset, points, n_clusters, axs[0])
+            singlelink(clean, dataset, points, n_clusters, axs[2])
         
 
 if __name__ == "__main__":
