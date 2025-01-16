dea0654c2a2eff44921f5bf3f7dcd743b2e1606a
Mario Lucassen <Mario.lucassen@hotmail.com>
2025-01-16 16:57:22 +0100

    Implemented experiments

--- greedy_submodular_runner.py
+++ greedy_submodular_runner.py
@@ -7,87 +7,180 @@ from time import time
 import findspark
 import pandas as pd
 
+def plot(balls, points, num_balls, size, minlim, maxlim):
+    colors = plt.get_cmap('viridis')(np.linspace(0, 1, len(balls)))
+    fig, ax = plt.subplots(figsize=(5, 5))
+    ax.set_title(f'Max cover set k = {k}, balls = {num_balls}, n = {size},')
+    plt.xlim(minlim, maxlim)
+    plt.ylim(minlim, maxlim)
 
-def main():
-    # k
-    k = 3
-    minlim = 0
-    maxlim = 10_000
+    for i, (x, y, r) in enumerate(balls):
+        circle = plt.Circle((x, y), r, color=colors[i])
+        ax.add_patch(circle)
 
-    def generate_dataset(size):
-        # points
-        coords_x = np.random.randint(minlim, maxlim, size)
-        coords_y = np.random.randint(minlim, maxlim, size)
-        return coords_x, coords_y
+    x, y = zip(*points)
+    ax.scatter(x, y)
+    plt.show(block = False)
 
-    def plot(balls, points, num_balls, size):
-        colors = plt.get_cmap('viridis')(np.linspace(0, 1, len(balls)))
-        fig, ax = plt.subplots(figsize=(5, 5))
-        ax.set_title(f'Max cover set k = {k}, balls = {num_balls}, n = {size},')
-        plt.xlim(minlim, maxlim)
-        plt.ylim(minlim, maxlim)
+def generate_circle(minlim, maxlim, maxradius):
+    x = np.random.randint(minlim, maxlim)
+    y = np.random.randint(minlim, maxlim)
+    r = np.random.randint(1, maxradius)
+    return (x, y, r)
 
-        for i, (x, y, r) in enumerate(balls):
-            circle = plt.Circle((x, y), r, color=colors[i])
-            ax.add_patch(circle)
+def generate_circles(circles_n, minlim, maxlim):
+    circless = set()
+    for i in range(circles_n):
+        circle = generate_circle(minlim, maxlim, maxlim / 50)
+        circless.add(circle)
+    return circless
 
-        x, y = zip(*points)
-        ax.scatter(x, y)
-        plt.show(block = False)
+def generate_dataset(size, minlim, maxlim) -> tuple[list, list]:
+    coords_x = np.random.randint(minlim, maxlim, size)
+    coords_y = np.random.randint(minlim, maxlim, size)
+    return (list(coords_x), list(coords_y))
 
-    timings = []
-    # sizes = []
-    conf = SparkConf('local[]').setAppName('GreedySubmodular').set("spark.executor.memory", "4g").set("spark.driver.memory", "2g")
-    sc = SparkContext.getOrCreate(conf=conf)
+def augment_dataset(coordsx, coordsy, number_of_points_to_add, minlim, maxlim):
+    coords_x, coords_y = generate_dataset(number_of_points_to_add, minlim, maxlim)
+    return coords_x + coordsx, coords_y + coordsy
 
-    # initialze pyspark (dry run)
+def save_experiment(name, timings):
+    df = pd.DataFrame(timings)
+    df.to_csv(name)
+
+def spark_dry_run(sc):
     balls = set[tuple[int, int, int]]([(100, 300, 400), (5000, 5000, 1000), (800, 300, 500), (2000, 300, 500), (2000, 7000, 500), (7000, 2000, 1000), (3000, 4000, 1500), (1000, 8000, 3000)])
     coords_x, coords_y = generate_dataset(100)
     alg = GreedySubmodularV2(sc, coords_x, coords_y)
-    O_balls, O_points = alg.run(k, balls)
+    _ = alg.run(3, balls)
 
-    balls_n = 50
-    ballss = set()
-    for i in range(balls_n):
-        x = np.random.randint(minlim, maxlim)
-        y = np.random.randint(minlim, maxlim)
-        r = np.random.randint(1, maxlim / 50)
-        ballss.add((x, y, r))
+def run_greedy_submodular(sc, coords_x, coords_y, k, circles):
+    alg = GreedySubmodularV2(sc, coords_x.copy(), coords_y.copy())
+    t0 = time()
+    _ = alg.run(k, circles.copy())
+    t1 = time()
+    return t1 - t0
+
+def experiment1(sc):
+    '''
+    Experiment with a constant circles set with varying positions and sizes. 
+    However, the size of the point set increases by 1000 points, 100 times.
+    Furthermore, this is done k times.
+    '''
+
+    circles_n = 50
+    minlim = 0
+    maxlim = 10_000
+    circless = generate_circles(circles_n, minlim, maxlim)
+
+    timings = []
 
     ks = 10
     for k in range(1, ks + 1):
         for i in range(100):
-            # x, y, r
-            balls = ballss.copy()
-            num_balls = len(balls)
+            circles = circless.copy()
 
             i = i + 1
             size = i * 1000 
             coords_x, coords_y = generate_dataset(size)
 
-            # algorithm
-            alg = GreedySubmodularV2(sc, coords_x, coords_y)
-            t0 = time()
-            _ = alg.run(k, balls)
-            t1 = time()
-            timings.append({ 'size': size, 'timing': t1 - t0, 'k': k })
-            print(f'k = {k}, size = {size}, timing = {t1 - t0}')
+            timing = run_greedy_submodular(sc, coords_x, coords_y, k, circles)
 
-    df = pd.DataFrame(timings)
-    df.to_csv('timings_v2k.csv')
+            timings.append({ 'size': size, 'timing': timing, 'k': k })
+            print(f'k = {k}, size = {size}, timing = {timing}')
 
-    # colors = plt.get_cmap('viridis')(np.linspace(0, 1, ks))
-    # # plot performance graph
-    # fig = plt.figure(figsize=(5, 5))
-    # df['k'] = df['k'] - 1
-    # plt.title(f'Running time vs data set size')
-    # plt.plot(df['size'], df['timing'], c=colors[df['k']])
-    # plt.xlabel('size')
-    # plt.ylabel('seconds')
-    # fig.savefig('performancev2k.png')
+    return timings
+    
+def experiment2(sc):
+    '''
+    Experiment with constant number of points but increasing number of circles
+    '''
+    k = 5
+    points_n = 10_000
+    minlim = 0
+    maxlim = 10_000
 
-    # # show all figs
-    # plt.show(block = True)
+    (coords_x, coords_y): tuple[list, list] = generate_dataset(points_n)
+    circless = generate_circles(k, minlim, maxlim)
+
+    timings = []
+    for i in range(k, 100):
+        if i > k:
+            circless.add(generate_circle(minlim, maxlim, maxlim / 50))
+
+        timing = run_greedy_submodular(sc, coords_x, coords_y, k, circless)
+
+        timings.append({ 'size': points_n, 'timing': timing, 'k': k, 'circles': len(circless) })
+        print(f'k = {k}, size = {points_n}, timing = {timing}, circles = {len(circless)}')
+
+    return timings
+
+def experiment3(sc):
+    '''
+    Experiment with constant number of points but increasing number of circles (randomized)
+    '''
+    k = 15
+    points_n = 10_000
+    minlim = 0
+    maxlim = 10_000
+
+    (coords_x, coords_y): tuple[list, list] = generate_dataset(points_n)
+    
+    timings = []
+    for i in range(k, 100):
+        circless = generate_circles(i, minlim, maxlim)
+
+        timing = run_greedy_submodular(sc, coords_x, coords_y, k, circless)
+
+        timings.append({ 'size': points_n, 'timing': timing, 'k': k, 'circles': len(circless) })
+        print(f'k = {k}, size = {points_n}, timing = {timing}, circles = {len(circless)}')
+
+    return timings
+
+def experiment4(sc):
+    '''
+    Experiment with number of points and circles increasing in a ratio of 5000:1 (lol)
+    '''
+    k = 5
+    points_n = 10_000
+    minlim = 0
+    maxlim = 10_000
+
+    (coords_x, coords_y): tuple[list, list] = generate_dataset(points_n)
+    circless = generate_circles(k, minlim, maxlim)
+
+    timings = []
+    for i in range(50):
+        if i > 0:
+            coords_x, coords_y = augment_dataset(coords_x, coords_y, 5000, minlim, maxlim)
+            circless.add(generate_circle(minlim, maxlim, maxlim / 50))
+            
+        timing = run_greedy_submodular(sc, coords_x, coords_y, k, circless)
+
+        timings.append({ 'size': points_n, 'timing': timing, 'k': k, 'circles': len(circless) })
+        print(f'k = {k}, size = {points_n}, timing = {timing}, circles = {len(circless)}')
+
+    return timings
+
+def main():
+    timings = []
+    conf = SparkConf('local[]').setAppName('GreedySubmodular').set("spark.executor.memory", "4g").set("spark.driver.memory", "2g")
+    sc = SparkContext.getOrCreate(conf=conf)
+
+    # initialze pyspark (dry run) to remove initialization overhead in performance comparisons
+    spark_dry_run()
+
+    # timings = experiment1()
+    # save_experiment('experiment1.csv', timings)
+
+    timings = experiment2(sc)
+    save_experiment('experiment2.csv', timings)
+
+    timings = experiment3(sc)
+    save_experiment('experiment3.csv', timings)
+
+    timings = experiment4(sc)
+    save_experiment('experiment4.csv', timings)
 
 
 if __name__ == "__main__":
